bc5647bbd5e4a755bea9a5ef6f65956dcdaaf45c
test <test@test.com>
2020-10-20 20:41:22 +0900

    시작

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+class LoginPage extends Component{
+    render(){
+        return(
+
+            <View>
+
+                <Text>안녕하세요</Text>
+            </View>
+
+        )
+    }
+}
+
+export default LoginPage;
