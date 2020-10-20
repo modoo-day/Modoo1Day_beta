642782c8c17af6a0a802443cec3ec26eb0d93f40
20173163 <wjddn494@naver.com>
2020-10-20 21:38:35 +0900

    LoginPage/Title

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -4,12 +4,9 @@ import {View, Text} from 'react-native';
 class LoginPage extends Component{
     render(){
         return(
-
             <View>
-
                 <Text>안녕하세요</Text>
             </View>
-
         )
     }
 }
--- src/components/LoginPage/title/title.js
+++ src/components/LoginPage/title/title.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 
-const exm = () =>{
-    return(
-        <View></View>
-    )
+function Title() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../../assets/icons/modoo.png')}
+        style={styles.image}
+      />
+      <Text style={styles.text}>모두의 하루</Text>
+    </View>
+  );
 }
 
-export default exm ;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '12%',
+    height: '12%',
+    paddingHorizontal: '10%',
+    marginHorizontal: '3%',
+  },
+  text: {
+    fontSize: 40,
+    marginHorizontal: '3%',
+  },
+});
+
+export default Title;
