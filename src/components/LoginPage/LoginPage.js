4aaf60b4f14e85482311505f57978503e0c0d91f
YoungHyun <yallee8@gmail.com>
2020-10-21 12:50:56 +0900

    MERGED TEMP

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,12 +2,15 @@
 import React, {Component} from 'react';
 import {View, Text, TextInput, Button} from 'react-native';
 import Bottom from './Bottom/Bottom';
-import Body from './body/Body';
+import Body from './Body/Body';
+import Title from "./Title/Title";
+
 
 class LoginPage extends Component {
   render() {
     return (
-      <View>
+      <View style={{flex:1}}>
+        <Title />
         <Body />
         <Bottom />
       </View>
--- src/components/LoginPage/body/Body.js
+++ src/components/LoginPage/body/Body.js
@@ -1,77 +1,43 @@
-import React, { Component } from 'react';
-import { Text, Alert, Button, View, StyleSheet, Image } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import React, {Component, useState} from 'react';
+import {Text, Button, View, StyleSheet, Image} from 'react-native';
+import {TextInput} from 'react-native-paper';
 
-export default class Body extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+const Body = function() {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
 
-  go = () => {
-           const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            if (reg.test(this.state.email) === true){
-               alert('valid');
-           }
-           else{
-               alert();
-           }
- 
-  }
-  
-  onLogin() {
-    const { username, password } = this.state;
-
-    Alert.alert('Credentials', `${username} + ${password}`);
-  }
-
-  render() {
-    return (
+  return (
     <>
-       
-        
-
-
       <View style={styles.container}>
-      <Text style={styles.inputext}>Sample Login Form</Text>
+        <Text style={styles.inputext}>임시 로그인 폼</Text>
         <TextInput
-          value={this.state.username}
-          onChangeText={(username) => this.setState({ username })}
-           label='Email'
+          value={id}
+          onChangeText={(username) => setId(username)}
+          label="Email"
           style={styles.input}
         />
         <TextInput
-          value={this.state.password}
-          onChangeText={(password) => this.setState({ password })}
-          label='Password'
+          value={pw}
+          onChangeText={(password) => setPw(password)}
+          label="Password"
           secureTextEntry={true}
           style={styles.input}
         />
-        
+
         <Button
           title={'Login'}
           style={styles.input}
-          onPress={this.onLogin.bind()}
+          //onPress={}
         />
       </View>
-
-      </>
-    );
-  }
-}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-  
-  },
-  input: {
-    
-  },
-  inputext: {
-    
-  },
+  container: {},
+  input: {},
+  inputext: {},
 });
+
+export default Body;
