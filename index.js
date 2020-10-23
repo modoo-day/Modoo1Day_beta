73427d199fc8cdd2e27606255cee62314673a74c
YoungHyun <yallee8@gmail.com>
2020-10-23 14:28:53 +0900

    For Merge

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import SignUp from './src/components/SignUp/SignUp'
-import LoginPage from './src/components/LoginPage/LoginPage'
+import SignUp from './src/components/SignUp/SignUp';
+import LoginPage from './src/components/LoginPage/LoginPage';
+import MarketPage from './src/components/MarketPage/MarketPage';
 
-AppRegistry.registerComponent(appName, () => SignUp);
+AppRegistry.registerComponent(appName, () => MarketPage);
 
