1add6701c3abb3156f6e416923de6571b218b842
wjddn494 <wjddn494@naver.com>
2020-10-28 22:25:01 +0900

    init

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import EmailCheck from './src/components/LoginPage/SignUp/EmailCheck';
+import Preference from './src/components/LoginPage/SignUp/Preference';
 
-AppRegistry.registerComponent(appName, () => EmailCheck);
+AppRegistry.registerComponent(appName, () => Preference);
